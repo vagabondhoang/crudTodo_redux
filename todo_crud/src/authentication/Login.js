1102df302a24946f556323957d948c3245e27000
hoang tuan anh <hoangtuananhnamdinh@gmail.com>
2017-11-06 14:25:00 +0700

    add login page

---
+++ todo_crud/src/authentication/Login.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleSubmit(e) {
+    e.preventDefault();
+    const { email, password } = this.state;
+    
+    if(email === 'hoangtuananhnamdinh@gmail.com' && password === 'abc') {
+      localStorage.setItem('email', email);
+      window.location.replace('http://localhost:3001/');
+    } else {
+      alert('Email or password is not correct')
+    }
+    
+  }
+  render() {
+    return (
+      <form>
+        <label htmlFor="email">Email</label>
+        <input  onChange={(e) => this.setState({ email: e.target.value })} name="email" />
+        <label htmlFor="password"></label>
+        <input type="password" onChange={(e) => this.setState({ password: e.target.value })} name="password" />
+        <button type="submit" onClick={this.handleSubmit}>Login</button>
+      </form>
+    )
+  }
+}
+
+export default Login;
--- todo_crud/src/index.js
+++ todo_crud/src/index.js
@@ -3,17 +3,37 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import rootReducer from './reducers';
 import './index.css';
 import App from './App';
+import Login from './authentication/Login.js';
+
 import registerServiceWorker from './registerServiceWorker';
 
+const isLoggedIn = () => {
+  let email;
+  email = localStorage.getItem('email');
+  
+  if(email !== null) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact  path='/' render={() => isLoggedIn() ? <App /> : <Redirect to='/login' />} /> 
+        <Route  path='/login'
+          render={() => isLoggedIn() ? <Redirect to="/" /> : <Login />}
+        />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
  document.getElementById('root'));
 
